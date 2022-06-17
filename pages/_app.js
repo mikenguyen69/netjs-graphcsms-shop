9329d40d519bdb148b2b39876ff172b86daf3642
mike.nguyen <mike.nguyen@xero.com>
2022-06-18 08:11:37 +1200

    Add graphql and getProducts query

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import { ChakraProvider, Flex, Box } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Flex w="full" minH="100vh" bgColor="gray.100">
+      <Box maxW="70vw" m="auto">
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </Box>
+    </Flex>
+  );
 }
 
-export default MyApp
+export default MyApp;
